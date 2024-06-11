d834cf0ad316387b4a5cb0d456870e7a54d64373
Gugpo <poliveiragpo@gmail.com>
2024-06-11 15:15:36 -0300

    Configuração e importações do Environment e Service para o front-end conversar com a API

--- projbarberfullstack.client/src/app/services/scheduleService/schedule.service.ts
+++ projbarberfullstack.client/src/app/services/scheduleService/schedule.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScheduleService {
 
-  private apiUrl = '${http://localhost:3000/api/schedule}';
+  private apiUrl = '${environment.ApiUrl}/schedule';
   constructor(private http: HttpClient) { }
 }
